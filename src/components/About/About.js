991d3fae607e527697d95caa6adabaeecaeb45e6
vermelon <bowernika@gmail.com>
2021-03-05 16:17:56 -0500

    updating about info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,9 @@ function About() {
         <div className = "about__picture"></div>
         <div className = "about__container">
         <h3 className = "about__header">About the author</h3>
-        <div className = "about__text"><p>This block describes the project author. 
-        Here you should indicate your name, what you do, and which development technologies you know.</p>
-       <p>You can also talk about your experience with Practicum, what you learned there, 
-        and how you can help potential customers.</p></div>
+        <div className = "about__text"><p>Veronika is an aspiring web developer. 
+        She completed Practicum by Yandex program in March 2021.</p>
+       <p>Veronika has experience in Quality Assurance and believes it can help in her future web development career.</p></div>
         </div>
         
     </div>
