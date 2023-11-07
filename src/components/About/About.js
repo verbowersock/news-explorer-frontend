ccf01e82ff4ec51c21b7f216a3b969ba09e3dff2
Veronika Bowersock <veronikabowersock@gmail.com>
2023-11-07 10:38:50 -0500

    Update About.js
    Updated about description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,9 +6,9 @@ function About() {
         <div className = "about__picture"></div>
         <div className = "about__container">
         <h3 className = "about__header">About the author</h3>
-        <div className = "about__text"><p>Veronika is an aspiring web developer. 
-        She completed Practicum by Yandex program in March 2021.</p>
-       <p>Veronika has experience in Quality Assurance and believes it can help in her future web development career.</p></div>
+        <div className = "about__text"><p>Veronika is an experienced web developer. She completed multiple web projects for a consulting company Sparq.</p>
+       <p>Veronika is passionate about Javascript, React, React Native, Node and web development in general. Please see more <a href="https://veronikabowersock.vercel.app/" target="_blank"
+                rel="noreferrer"> here</a> </p></div>
         </div>
         
     </div>
